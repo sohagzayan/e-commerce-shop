d7940c822e9e29915df4edf5e5a587ac333f02c1
sohag.zayan <gsohag360@gmail.com>
2022-11-29 21:40:17 -0800

    add product filter and search sort etc

--- backend/controllers/productControllers.js
+++ backend/controllers/productControllers.js
@@ -9,9 +9,25 @@ exports.createProduct = tryCatch(async (req, res, next) => {
 });
 
 // get all product  -> admin
+
 exports.getAllProduct = tryCatch(async (req, res, next) => {
-  const products = await Product.find();
-  res.status(200).json({ success: true, products });
+  const resultPerPage = 8;
+  const productsCount = await Product.countDocuments();
+  const apiFeature = new ApiFeatures(Product.find(), req.query)
+    .search()
+    .filter();
+
+  let products = await apiFeature.query;
+  let filteredProductsCount = products.length;
+  apiFeature.pagination(resultPerPage);
+  products = await apiFeature.query;
+  res.status(200).json({
+    success: true,
+    products,
+    productsCount,
+    resultPerPage,
+    filteredProductsCount,
+  });
 });
 
 //  update product -> admin
