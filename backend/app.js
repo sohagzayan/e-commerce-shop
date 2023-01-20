0119a7a33f9d3200451efd568401fa21512a9db3
sohag.zayan <gsohag360@gmail.com>
2023-01-20 11:17:09 -0800

    final try

--- backend/app.js
+++ backend/app.js
@@ -27,9 +27,9 @@ app.use("/api/v1", user);
 app.use("/api/v1", order);
 app.use("/api/v1", payment);
 
-app.use(express.static(path.join(__dirname, "../frontend")));
+app.use(express.static(path.join(__dirname, "../frontend/build")));
 app.get("*", (req, res) => {
-  res.sendFile(path.resolve(__dirname, "../frontend/public/index.html"));
+  res.sendFile(path.resolve(__dirname, "../frontend/build/index.html"));
 });
 // middleware  error handler
 app.use(errorHandlerGard);
